352cf01d39c725a6cff4c3a793cebf1b71674fd2
fingerbirdy <fingerbirdy@gmail.com>
2022-01-18 19:45:53 -0500

    GUI Setting system

--- src/main/java/com/fingerbirdy/highways/forgtools/gui/Settings.java
+++ src/main/java/com/fingerbirdy/highways/forgtools/gui/Settings.java
@@ -1,23 +1,38 @@
 package com.fingerbirdy.highways.forgtools.gui;
 
+import com.fingerbirdy.highways.forgtools.FileSystem;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 
-import java.nio.file.Files;
-
 public class Settings {
+
+    private static final String defaults = "{'hud':{'watermark':{'enabled':0}}}";
 
     public static JsonObject gui = new JsonObject();
 
     public static void load() {
 
+        JsonParser parser = new JsonParser();
+        String rawJson = FileSystem.readAll("GUI_SETTINGS");
+        if (rawJson == null) {
+            gui = parser.parse(defaults).getAsJsonObject();
+        } else {
 
+            try {
+                gui = parser.parse(rawJson).getAsJsonObject();
+            } catch (Exception e) {
+                gui = parser.parse(defaults).getAsJsonObject();
+            }
+
+        }
+
+        save();
 
     }
 
     public static void save() {
 
-
+        FileSystem.save("GUI_SETTINGS", gui.toString());
 
     }
 
